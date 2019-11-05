90183f7dbe8b568a85d94751c3f8e7c8564888e5
superbogy <mulberry10th@gmail.com>
2019-11-05 23:49:44 +0800

    feat: create and update app api test case

--- tests/api/app.py
+++ tests/api/app.py
@@ -1,9 +1,8 @@
+import uuid
 from bson import ObjectId
 from tests.basecase import BaseTestCase
-from unittest import mock
+from unittest.mock import patch
 from eclogue.models.application import Application
-from eclogue.lib.integration import Integration
-
 
 
 class AppTest(BaseTestCase):
@@ -40,18 +39,54 @@ class AppTest(BaseTestCase):
 
     def test_add_app(self):
         data = self.get_data('application')
+        data['name'] = str(uuid.uuid4())
         url = self.get_api_path('/apps')
         body = self.body(data)
         response = self.client.post(url, data=self.body({}), headers=self.jwt_headers)
         self.assert400(response)
         self.assertResponseCode(response, 174000)
-        with mock.patch('eclogue.lib.integration.Integration', app_type='test', app_params={}) as mock_instance:
-            mock_ret = mock_instance.return_value
-            mock_ret.check_app_params.return_value = False
-            print(Integration('test', {}) is mock_ret)
+        with patch('eclogue.api.app.Integration') as mockClass:
+            instance = mockClass.return_value
+            instance.check_app_params.return_value = False
+            response = self.client.post(url, data=body, headers=self.jwt_headers)
+            self.assert400(response)
+            self.assertResponseCode(response, 174003)
+            instance.check_app_params.return_value = True
+            clone = data.copy()
+            clone.pop('name')
+            response = self.client.post(url, data=self.body(clone), headers=self.jwt_headers)
+            self.assert400(response)
+            self.assertResponseCode(response, 174001)
+            response = self.client.post(url, data=body, headers=self.jwt_headers)
+            self.assert200(response)
+            response = self.client.post(url, data=body, headers=self.jwt_headers)
+            self.assert400(response)
+            self.assertResponseCode(response, 174002)
 
-            # response = self.client.post(url, data=body, headers=self.jwt_headers)
-            # print('mmmmmmmcok', response.json)
-        # self.assert200(response)
-        Application.delete_one({'name': data['name']})
+        Application().collection.delete_many({'name': data['name']})
+
+    def test_update_app(self):
+        data = self.get_data('application')
+        data['name'] = str(uuid.uuid4())
+        result = Application.insert_one(data.copy())
+        app_id = result.inserted_id
+        with patch('eclogue.api.app.Integration') as mockClass:
+            instance = mockClass.return_value
+            instance.check_app_params.return_value = False
+            url = self.get_api_path('/apps/' + str(app_id))
+            response = self.client.put(url, data=self.body(data), headers=self.jwt_headers)
+            self.assert400(response)
+            self.assertResponseCode(response, 174003)
+            instance.check_app_params.return_value = True
+            response = self.client.put(url, data=self.body({}), headers=self.jwt_headers)
+            self.assert400(response)
+            self.assertResponseCode(response, 174000)
+            invalid_url = self.get_api_path('/apps/' + str(ObjectId()))
+            response = self.client.put(invalid_url, data=self.body(data), headers=self.jwt_headers)
+            self.assert404(response)
+            self.assertResponseCode(response, 104040)
+            response = self.client.put(url, data=self.body(data), headers=self.jwt_headers)
+            self.assert200(response)
+
+        Application().collection.delete_one({'_id': app_id})
 
